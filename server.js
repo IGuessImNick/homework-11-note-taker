a4882c8f08865a1d7a428da1b661817b485c6aa3
Nicholas Skinner <njskinner@ucdavis.edu>
2021-09-08 14:45:41 -0700

    initial functionality

---
+++ server.js
@@ -0,0 +1,64 @@
+// =================================================
+// DEPENDENCIES
+// =================================================
+const express = require("express");
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 1001;
+
+// =================================================
+// MIDDLEWEAR
+// =================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// =================================================
+// ROUTING
+// =================================================
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
+
+// gets notes and saves them to db.json
+app.get("api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+// Delete method for deleting note with specified uuid
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+});
+
+// html call for homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// html call for notes.html
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// =================================================
+// LISTENER
+// =================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT)
+});
